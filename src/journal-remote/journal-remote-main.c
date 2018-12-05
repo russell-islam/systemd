52bf5109fc35b2a19776b9318f3be948dab1a956
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-12-05 22:45:02 +0100

    journald: set a limit on the number of fields
    The fix for CVE-2018-16865 is plucked from two commits that have
    been pushed to systemd master.
    
    journald: set a limit on the number of fields (1k)
    
    We allocate a iovec entry for each field, so with many short entries,
    our memory usage and processing time can be large, even with a relatively
    small message size. Let's refuse overly long entries.
    
    CVE-2018-16865
    https://bugzilla.redhat.com/show_bug.cgi?id=1653861
    
    What from I can see, the problem is not from an alloca, despite what the CVE
    description says, but from the attack multiplication that comes from creating
    many very small iovecs: (void* + size_t) for each three bytes of input message.
    
    Patch backported from systemd master at
    052c57f132f04a3cf4148f87561618da1a6908b4.
    
    journal-remote: set a limit on the number of fields in a message
    
    Existing use of E2BIG is replaced with ENOBUFS (entry too long), and E2BIG is
    reused for the new error condition (too many fields).
    
    This matches the change done for systemd-journald, hence forming the second
    part of the fix for CVE-2018-16865
    (https://bugzilla.redhat.com/show_bug.cgi?id=1653861).
    
    Patch backported from systemd master at
    ef4d6abe7c7fab6cbff975b32e76b09feee56074.
    with the changes applied by 7fdb237f5473cb8fc2129e57e8a0039526dcb4fd
    removed.
    
    CVE: CVE-2018-16865
    Upstream-Status: Backport
    Signed-off-by: Marcus Cooper <marcusc@axis.com>

--- src/journal-remote/journal-remote-main.c
+++ src/journal-remote/journal-remote-main.c
@@ -212,7 +212,12 @@ static int process_http_upload(
                         break;
                 else if (r < 0) {
                         log_warning("Failed to process data for connection %p", connection);
-                        if (r == -E2BIG)
+                        if (r == -ENOBUFS)
+                                return mhd_respondf(connection,
+                                                    r, MHD_HTTP_PAYLOAD_TOO_LARGE,
+                                                    "Entry is above the maximum of %u, aborting connection %p.",
+                                                    DATA_SIZE_MAX, connection);
+                        else if (r == -E2BIG)
                                 return mhd_respondf(connection,
                                                     r, MHD_HTTP_PAYLOAD_TOO_LARGE,
                                                     "Entry is too large, maximum is " STRINGIFY(DATA_SIZE_MAX) " bytes.");
